9a76411b4a56864da4ef4249947cd2c4605ad965
Tessa van der Heiden <tessavanderheiden@Tessas-MacBook-Pro.local>
2022-11-24 11:38:24 +0100

    exercise 7

--- lesson-3-data-validation/exercises/exercise_7/starter/test_data.py
+++ lesson-3-data-validation/exercises/exercise_7/starter/test_data.py
@@ -72,6 +72,7 @@ def test_class_names(data):
     # HINT: you can use the .isin method of pandas, and .all to check that the condition
     # is true for every row. For example, df['one'].isin(['a','b','c']).all() is True if
     # all values in column "one" are contained in the list 'a', 'b', 'c'
+    assert data['genre'].isin(known_classes).all()
 
 
 def test_column_ranges(data):
@@ -95,4 +96,4 @@ def test_column_ranges(data):
         # YOUR CODE HERE: check that the values in the column col_name are within the expected range
         # HINT: look at the .between method of pandas, and then use .all() like in the previous
         # test
-        pass
+        assert data[col_name].dropna().between(minimum, maximum).all()
